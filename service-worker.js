a768cab41731b0808a85832abbe374f75cd2ccfe
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2016-09-08 08:25:56 +0900

    cache all request

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,15 @@
+var cache_name = 'ichinomiya_map_1';
 self.addEventListener('fetch', function(event) {
-  console.log(event.request.url);
+  event.respondWith(
+    caches.open(cache_name).then(function(cache) {
+      return cache.match(event.request).then(function (response) {
+        return response || fetch(event.request).then(function(response) {
+          if (response.ok) {
+            cache.put(event.request, response.clone());
+          }
+          return response;
+        });
+      });
+    })
+  );
 });
